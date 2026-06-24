675dc7b7ad8f4c59aee4c105bb3ed580559567da
elyceria <elycileria@gmail.com>
2026-06-24 02:12:54 -0700

    i did not create slash commands last commit...

--- commands.js
+++ commands.js
@@ -1,10 +1,10 @@
-const OWNER_ID = "YOUR_DISCORD_USER_ID";
+const ELY_ID = "1308269381863739485";
 
 module.exports = {
   name: "teststream",
 
   async execute(interaction) {
-    if (interaction.user.id !== OWNER_ID) {
+    if (interaction.user.id !== ELY_ID) {
       return interaction.reply({
         content: "hm? you aren't elyceria, so you can't use this command! ask her if you want to use it with a pretty please on top, okay?",
         ephemeral: true
@@ -12,5 +12,31 @@ module.exports = {
     }
 
     return interaction.reply("good morning elyceria ~  bot affirmative!");
+  }
+};
+
+const { EmbedBuilder } = require("discord.js");
+
+const ELY_ID = "1308269381863739485";
+
+module.exports = {
+  name: "teststream",
+
+  async execute(interaction) {
+    if (interaction.user.id !== ELY_ID) {
+      return interaction.reply({
+        content: "hm? you aren't elyceria, so you can't use this command! ask her if you want to use it with a pretty please on top, okay?",
+        ephemeral: true
+      });
+    }
+
+    const embed = new EmbedBuilder()
+        .setDescription("##  ♡ 　⊹ 　「 knock knock~ is this test message working? 」\n⸝⸝ 　 ﹕　 elyceria is NOT live __right now__ playing __${tw.game}__ *!*\n\n﹒　　come watch the NONstream on twitch [here](${tw.lin}) *!*")
+        .setImage("https://i.ytimg.com/vi/sqMmUk69BVA/maxresdefault.jpg");
+
+    return interaction.reply({
+      embeds: [embed],
+      allowedMentions: { parse: [] } // extra safety: prevents accidental pings
+    });
   }
 };
---
+++ deploy-commands.js
@@ -0,0 +1,25 @@
+const {REST,Routes,SlashCommandBuilder} = require("discord.js");
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("teststream")
+    .setDescription("hmm, i wonder if this thing is on?")
+    .toJSON()
+];
+
+const rest = new REST({version: "10"}).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("initializing slash commands . . .");
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log("slash commands initialized successfully ~");
+  } catch (error) {
+    console.error(error);
+  }
+})();
